bcfbc2c88bbd7f71354052be5522b29a416035d3
Elodie Royant <eroyant@gmail.com>
2018-01-30 13:03:41 +0100

    set up index.html and express

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require("express");
+
 const app = express();
+
+app.use(express.static('asset'))
+
+// app.get("/", (req, res) => {
+//     res.send({ hello: "world" });
+// });
+
 app.get("/", (req, res) => {
-    res.send({ hello: "world" });
+  res.sendFile((path.join(__dirname, "index.html"));
 });
+
 const PORT = process.env.PORT;
+
 app.listen(PORT);
