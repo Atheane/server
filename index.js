8082f68b6a6dea9dec3817548be9fc585d7f1ece
Elodie Royant <eroyant@gmail.com>
2018-01-30 14:08:02 +0100

    fixing index.js for hopefully, an html page (path forgotten)

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
 app.use(express.static('asset'))
 
 app.get("/", (req, res) => {
-  res.sendFile((path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 
