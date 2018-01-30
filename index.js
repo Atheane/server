12d61c2750adc4868036b816842481a8f78d010f
Elodie Royant <eroyant@gmail.com>
2018-01-30 13:18:09 +0100

    fixing index.js for at least, an hello world

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const app = express();
 
 app.use(express.static('asset'))
 
-// app.get("/", (req, res) => {
-//     res.send({ hello: "world" });
-// });
-
 app.get("/", (req, res) => {
-  res.sendFile((path.join(__dirname, "index.html"));
+    res.send({ hello: "world" });
 });
+
+// app.get("/", (req, res) => {
+//   res.sendFile((path.join(__dirname, "index.html"));
+// });
 
 const PORT = process.env.PORT;
 
