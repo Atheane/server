367cf154e62a45e3a57dfb467b1939f84e7df222
Elodie Royant <eroyant@gmail.com>
2018-01-30 14:06:32 +0100

    fixing index.js for hopefully, an html page (path forgotten)

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const express = require("express");
-
 const app = express();
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT;
 
-app.use(express.static('asset'))
 
 app.get("/", (req, res) => {
     res.send({ hello: "world" });
 });
 
-// app.get("/", (req, res) => {
-//   res.sendFile((path.join(__dirname, "index.html"));
-// });
+app.use(express.static('asset'))
 
-const PORT = process.env.PORT;
+app.get("/", (req, res) => {
+  res.sendFile((path.join(__dirname, "index.html"));
+});
+
 
 app.listen(PORT);
